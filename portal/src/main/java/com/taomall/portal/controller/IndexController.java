01b2f47b680a36004224b48303aac9adc22b9ab3
zonway <3126599489@qq.com>
2018-03-23 13:41:56 +0800

    update

--- portal/src/main/java/com/taomall/portal/controller/IndexController.java
+++ portal/src/main/java/com/taomall/portal/controller/IndexController.java
@@ -2,9 +2,12 @@ package com.taomall.portal.controller;
 
 import com.taomall.portal.services.ContentServie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by zhoun on 2018/3/20.
@@ -20,6 +23,15 @@ public class IndexController {
         String adJson = contentServie.getContentList();
         model.addAttribute("ad1", adJson);
         return "index";
+    }
+
+    @RequestMapping(value = "/httpclient/post", method = RequestMethod.POST,
+            produces = MediaType.TEXT_PLAIN_VALUE + ";charset=utf-8")
+    @ResponseBody
+    public String testPost(String username, String password) {
+        String result = "username:" + username + "\tpassword:" + password;
+        System.out.println(result);
+        return "username:" + username + ",password:" + password;
     }
 
 }
